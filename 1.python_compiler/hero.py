ced5360fa7396cb91d6e7035803c019d2cbe9eb4
yingshaoxo <yingshaoxo@gmail.com>
2023-04-26 18:13:46 +0800

    today I have implemented hero init, add, remove, compile function. now it is able to use golang package without any problems

--- 1.python_compiler/hero.py
+++ 1.python_compiler/hero.py
@@ -3,14 +3,19 @@
 
 # Run this to generate bash auto complete script: Tools -- --completion
 
+import json
+from platform import architecture
+import re
+
 from auto_everything.python import Python #type: ignore
 from auto_everything.disk import Disk #type: ignore
 from auto_everything.io import IO #type: ignore
-from auto_everything.terminal import Terminal #type: ignore
+from auto_everything.terminal import Terminal, Terminal_User_Interface #type: ignore
 python = Python()
 disk = Disk()
 io_ = IO()
 terminal = Terminal()
+terminal_user_interface = Terminal_User_Interface()
 
 import hero_to_golang
 
@@ -22,6 +27,17 @@ import hero_to_golang
 class Hero():
     def __init__(self):
         self.hero_to_golang_compiler = hero_to_golang.HeroToGolangCompiler()
+        self.current_working_directory = disk.get_current_working_directory()
+
+        self.package_json_file_path = disk.join_paths(self.current_working_directory, "package.json")
+        self.has_package_json_file = disk.exists(self.package_json_file_path)
+        if (self.has_package_json_file):
+            self.package_object = json.loads(io_.read(self.package_json_file_path))
+
+        self.output_golang_folder = disk.join_paths(self.current_working_directory, "build", "golang")
+        self.output_golang_mod_path = disk.join_paths(self.output_golang_folder, "go.mod")
+
+        self.binary_output_folder = disk.join_paths(self.current_working_directory, "build", "binary")
 
     def hi(self):
         print("""
@@ -34,16 +50,26 @@ class Hero():
         """
         pass
 
-    def run(self, file: str):
+    def run(self, file: str | None = None):
         """
         file: 
             Code file, like 'xx.hero'
         """
+        output_folder = disk.get_a_temp_folder_path()
+
+        if file == None and self.has_package_json_file:
+            file = self.package_object.get("main")
+            output_folder = self.output_golang_folder
+
+        if file == None:
+            print(f"""Sorry, package.json doesn't have an entry point setting, it should have a pair like {"main": "main.hero"}""")
+            return
+
         file = disk.get_absolute_path(path=file)
         directory_path = disk.get_directory_path(path=file)
 
-        # output_folder = disk.get_a_temp_folder_path()
-        output_folder = "/home/yingshaoxo/CS/hero/playground/0.first_dev_phase/run_compiling_output"
+        disk.create_a_folder(output_folder)
+
         golang_file_path = self.hero_to_golang_compiler.compile_to_golang_file(input_base_folder=directory_path, input_file=file, output_folder=output_folder)
         binary_file_path = self.hero_to_golang_compiler.compile_to_binary_file(input_go_file=golang_file_path, output_binary_file=disk.get_a_temp_file_path("hero_run"))
         terminal.run(f"""
@@ -52,21 +78,91 @@ class Hero():
         echo "cd {disk.get_directory_path(golang_file_path)}"
         echo "vim {golang_file_path}"
         echo ""
-        echo ""
         echo "_________________________"
         echo ""
         echo ""
         {binary_file_path}
         """)
 
-    def compile(self, file: str, platform: str):
+    def compile(self, file: str | None = None, platform: str | None = None, output: str | None = None):
         """
         file: 
             Code file, like 'xx.hero'
         platform:
             Operation system and archtecture, like 'linux/amd64' or 'linux/arm'
+            -->      darwin/386
+            -->    darwin/amd64
+            -->       linux/386
+            -->     linux/amd64
+            -->       linux/arm
+            -->     freebsd/386
+            -->   freebsd/amd64
+            -->     openbsd/386
+            -->   openbsd/amd64
+            -->     windows/386
+            -->   windows/amd64
+            -->     freebsd/arm
+            -->      netbsd/386
+            -->    netbsd/amd64
+            -->      netbsd/arm
+            -->       plan9/386
+        output:
+            The output executable file, like `./xx.run`
         """
-        pass
+        """
+        You can try to use gox to do the same thing: https://github.com/mitchellh/gox
+        """
+        output_folder = disk.get_a_temp_folder_path()
+
+        build_based_on_package_json = False
+        if file == None and self.has_package_json_file:
+            file = self.package_object.get("main")
+            output_folder = self.output_golang_folder
+            output = self.binary_output_folder
+            build_based_on_package_json = True
+
+        disk.create_a_folder(output_folder)
+
+        if output == None:
+            print(f"Sorry, you have to specify the output_folder like this: hero compile --file 'xx.hero' --output 'xx.run'")
+            return
+
+        if file == None:
+            print(f"""Sorry, package.json doesn't have an entry point setting, it should have a pair like {"main": "main.hero"}""")
+            return
+        file = disk.get_absolute_path(path=file)
+        directory_path = disk.get_directory_path(path=file)
+        pure_name_of_file, _ = disk.get_stem_and_suffix_of_a_file(file)
+
+        golang_file_path = self.hero_to_golang_compiler.compile_to_golang_file(input_base_folder=directory_path, input_file=file, output_folder=output_folder)
+
+        if platform != None:
+            operation_system, architecture = platform.split("/")
+        else:
+            operation_system, architecture = None, None
+
+        if build_based_on_package_json == True:
+            if platform == None:
+                output_binary_file = disk.join_paths(output, f"{pure_name_of_file}.run")
+            else:
+                output_binary_file = disk.join_paths(output, f"{pure_name_of_file}_{operation_system}_{architecture}.run")
+        else:
+            output_binary_file = disk.get_absolute_path(output)
+
+        binary_file_path = self.hero_to_golang_compiler.compile_to_binary_file(input_go_file=golang_file_path, output_binary_file=output_binary_file, operation_system=operation_system, architecture=architecture)
+
+        terminal.run(f"""
+        #clear
+        echo ""
+        echo "cd {disk.get_directory_path(golang_file_path)}"
+        echo "vim {golang_file_path}"
+        echo ""
+        echo "_________________________"
+        echo ""
+        echo ""
+        echo "{binary_file_path} "
+        echo ""
+        """)
 
     def convert(self, file: str, to: str):
         """
@@ -81,31 +177,170 @@ class Hero():
         """
         Create a template by using terminal UI, so that user can add/remove packages
         """
-        pass
+        if disk.exists(self.package_json_file_path):
+            print(f"Sorry, folder '{self.current_working_directory}' is not empty. It already has a 'package.json' file.")
+            return
+        else:
+            # json_content = """
+            # {
+            #     "name": "it_has_alternatives",
+            #     "version": "0.0.0",
+            #     "scripts": {
+            #         "dev": "vite",
+            #         "build": "vue-tsc && vite build",
+            #     },
+            #     "dependencies": {
+            #         "vue": "^3.2.47",
+            #     },
+            #     "devDependencies": {
+            #         "vite": "^4.2.0",
+            #     }
+            # }
+            # """
+            json_content = """
+            {
+                "name": "",
+                "version": "",
+                "author": "",
+                "scripts": {},
+                "main": "main.hero",
+                "dependencies": {},
+                "dev_dependencies": {},
+                "go_dependencies": {},
+                "go_dev_dependencies": {}
+            }
+            """
+            package_object = json.loads(json_content)
+            # pprint(package_object)
+            print("\n")
+
+            # handle project name
+            default_project_name = disk.get_directory_name(self.current_working_directory)
+            def assign_name(text: str):
+                package_object["name"] = text
+            terminal_user_interface.input_box(
+                f"Please give me a project name: (use '{default_project_name}' by default)___", 
+                default_value=default_project_name,
+                handle_function=assign_name
+            )
+
+            # handle project version
+            default_project_version = "0.0.0"
+            def assign_version(text: str):
+                package_object["version"] = text
+            terminal_user_interface.input_box(
+                f"Please give me a project version: (use '{default_project_version}' by default)___", 
+                default_value=default_project_version,
+                handle_function=assign_version
+            )
+
+            # handle author name
+            default_author = ""
+            def assign_author(text: str):
+                package_object["author"] = text
+            terminal_user_interface.input_box(
+                f"Please give me your name: (It's better be an email address)___", 
+                default_value=default_author,
+                handle_function=assign_author
+            )
+
+            io_.write(self.package_json_file_path, json.dumps(package_object, indent=4))
+
+    def install(self):
+        """
+        Install all package defined in package.json
+        """
+        if not self.has_package_json_file:
+            print("You have to run `hero init` first.")
+            return
+
+        go_dependencies = self.package_object.get("go_dependencies")
+        if go_dependencies == None:
+            print('package.json should have a pair called {"go_dependencies":{}}')
+            return
+        
+        for package_name in self.package_object.get("go_dependencies").keys(): #type: ignore
+            result = terminal.run_command(f"""go get {package_name}""", cwd=self.output_golang_folder)
+            print(result)
     
     def add(self, package_name: str, git_link: str | None = None):
         """
-        Add a package to project
+        Add a package to project:
+            `hero add github.com/google/uuid`
         """
-        pass
+        if not self.has_package_json_file:
+            print("You have to run `hero init` first.")
+            return
 
+        go_dependencies = self.package_object.get("go_dependencies")
+        if go_dependencies == None:
+            print('package.json should have a pair called {"go_dependencies":{}}')
+            return
+        
+        result = terminal.run_command(f"""
+        go get {package_name}
+        """, cwd=self.output_golang_folder)
+        if ": EOF".lower() in result.lower():
+            print(result)
+            return
+        else:
+            self.package_object['go_dependencies'][package_name] = ""
+            io_.write(self.package_json_file_path, json.dumps(self.package_object, indent=4))
+        
     def remove(self, package_name: str):
         """
-        Remove a package from project
+        Remove a package from project:
+            `hero remove github.com/google/uuid`
         """
-        pass
+        """
+        You could add a command line interface here, if package_name == None, let user choose what to delete
+        """
+        if not self.has_package_json_file:
+            print("You have to run `hero init` first.")
+            return
 
-    def update(self, package_name: str, git_link: str | None = None):
-        """
-        Update a package for this project
-        """
-        pass
+        go_dependencies = self.package_object.get("go_dependencies")
+        if go_dependencies == None:
+            print('package.json should have a pair called {"go_dependencies":{}}')
+            return
 
-    def update_all(self):
-        """
-        Update all package for this project
-        """
-        pass
+        if disk.exists(self.output_golang_mod_path):
+            def try_to_do_a_replace(match_obj: re.Match): #type: ignore
+                if match_obj.group() is not None:
+                    package_require_line = match_obj.group('package_require_line') #type: ignore
+                    if package_require_line != None:
+                        return "" 
+            golang_mod_file_content = io_.read(self.output_golang_mod_path)
+            result = re.sub(f"(?P<package_require_line>require {package_name}(?:.*)\s*)", try_to_do_a_replace, golang_mod_file_content, flags=re.MULTILINE) #type: ignore
+            io_.write(self.output_golang_mod_path, result) #type: ignore
+
+        result = terminal.run_command(f"""
+        go clean
+        go mod tidy
+        """, cwd=self.output_golang_folder)
+        if ": EOF".lower() in result.lower():
+            print(result)
+            return
+        else:
+            if package_name in self.package_object['go_dependencies']:
+                del self.package_object['go_dependencies'][package_name]
+                io_.write(self.package_json_file_path, json.dumps(self.package_object, indent=4))
+            else:
+                print(f"Sorry, package '{package_name}' not in:")
+                print("".join([f"* {one}\n" for one in self.package_object['go_dependencies'].keys()]))
+                return
+
+    # def update(self, package_name: str, git_link: str | None = None):
+    #     """
+    #     Update a package for this project
+    #     """
+    #     pass
+
+    # def update_all(self):
+    #     """
+    #     Update all package for this project
+    #     """
+    #     pass
 
 
 python.make_it_global_runnable(executable_name="hero")
--- 1.python_compiler/hero_to_golang.py
+++ 1.python_compiler/hero_to_golang.py
@@ -66,7 +66,7 @@ class HeroToGolangCompiler:
 
                 return golang_input_arguments_template
 
-        return re.sub(r"(?P<variable_type>\w+) (?P<variable_name>\w+)", try_to_do_a_replace, old_text, flags=re.MULTILINE) #type: ignore
+        return re.sub(r"(?P<variable_type>[\w\[\]\.]+) (?P<variable_name>\w+)", try_to_do_a_replace, old_text, flags=re.MULTILINE) #type: ignore
 
     def convert_hero_package_function_access_method_to_golang_package_function_access_method(self, old_text: str, package_name_list: list[str]) -> str:
         for package_name in package_name_list:
@@ -78,7 +78,7 @@ class HeroToGolangCompiler:
             old_text = re.sub(f"(?P<part1>{package_name}\.\s*)(?P<part2>\w)", try_to_do_a_replace, old_text, flags=re.MULTILINE) #type: ignore
         return str(old_text) #type: ignore
     
-    def change_hero_function_into_golang_function_format(self, old_text: str) -> str:
+    def change_hero_function_into_golang_function_format_by_using_regex_in_a_unsafe_way(self, old_text: str) -> str:
         def try_to_do_a_replace(match_obj: re.Match): #type: ignore
             if match_obj.group() is not None:
                 should_i_export_it = bool(match_obj.group("export") != None) #type: ignore
@@ -108,6 +108,82 @@ func {function_name}({function_input_arguments}) {function_return_type} {{
                 return golang_function_template
 
         return re.sub(r"^(?P<export>export )*(?P<async>async )*(?P<function_return_type>.*) (?P<function_name>.*)\((?P<function_input_arguments>.*)\) \{(?P<function_content>(?:\n|.)*?)\}", try_to_do_a_replace, old_text, flags=re.MULTILINE) #type: ignore
+
+    def find_next_code_block(self, text: str, start: int = 0) -> Tuple[int|None, int|None]:
+        search_balanced_curly_brackets_end = False
+        start_index = None
+        end_index = None
+        curly_brackets_start_symbol_count = 0
+        curly_brackets_end_symbol_count = 0
+        for index, char in enumerate(text):
+            if index < start:
+                continue
+            if search_balanced_curly_brackets_end == False:
+                if char == "{":
+                    search_balanced_curly_brackets_end = True
+                    start_index = index
+                    curly_brackets_start_symbol_count += 1
+                    continue
+            if char == "{":
+                curly_brackets_start_symbol_count += 1
+                continue
+            if search_balanced_curly_brackets_end == True:
+                if char == "}":
+                    curly_brackets_end_symbol_count += 1
+                    if curly_brackets_start_symbol_count == curly_brackets_end_symbol_count:
+                        end_index = index
+                        break
+        return start_index, end_index+1 #type: ignore
+
+    def change_hero_function_into_golang_function_format(self, old_text: str) -> str:
+        def try_to_do_a_replace(match_obj: re.Match): #type: ignore
+            if match_obj.group() is not None:
+                should_i_export_it = bool(match_obj.group("export") != None) #type: ignore
+                is_it_async_function = bool(match_obj.group("async") != None) #type: ignore
+                function_name = str(match_obj.group('function_name')) #type: ignore
+                function_input_arguments = str(match_obj.group('function_input_arguments')) #type: ignore
+                function_content = match_obj.group('function_content')#.strip('\n') #type: ignore
+                function_return_type = str(match_obj.group('function_return_type')) #type: ignore
+
+                if should_i_export_it == True:
+                    function_name = function_name.capitalize()
+
+                function_input_arguments = self.convert_hero_function_input_arguments_into_golang_function_input_arguments(function_input_arguments)
+
+                if function_return_type == 'void':
+                    function_return_type = ''
+                if function_name.lower() == 'main':
+                    function_return_type = ''
+                function_return_type = self.convert_hero_variable_type_into_golang_variable_type(function_return_type) #type: ignore
+
+                if function_return_type.strip() == "":
+                    golang_function_template = f"""
+    func {function_name}({function_input_arguments}) {{{function_content}}}
+                    """.strip()
+                else:
+                    golang_function_template = f"""
+    func {function_name}({function_input_arguments}) {function_return_type} {{{function_content}}}
+                    """.strip()
+
+                return golang_function_template
+
+        output_text = re.sub(r"^(?P<export>export )*(?P<async>async )*(?P<function_return_type>.*) (?P<function_name>.*)\((?P<function_input_arguments>.*)\) \{(?P<function_content>(?:\n|.)*?)\}", try_to_do_a_replace, old_text, flags=re.MULTILINE) #type: ignore
+
+        # # get real code_block if needed
+        # content_start_and_end_index_list: list[Tuple[int, int]] = []
+        # new_function_match_list = re.finditer(r"^func (?P<function_name>.*)\((?P<function_input_arguments>.*)\) \{(?P<function_content>(?:\n|.)*?)\}", output_text, flags=re.MULTILINE) #type: ignore
+        # for match in new_function_match_list:
+        #     if match.group() is not None: #type: ignore
+        #         function_name_start_index = match.start('function_name') #type: ignore
+        #         if function_name_start_index != -1:
+        #             code_block_start, code_block_end = self.find_next_code_block(output_text, start=function_name_start_index) #type: ignore
+        #             if code_block_start != None and code_block_end != None:
+        #                 code_block = output_text[code_block_start:code_block_end] #type: ignore
+        #                 content_start_and_end_index_list.append((code_block_start,code_block_end))
+        #                 print(code_block) #type: ignore
+        #                 print("\n\n--------------\n\n")
+
+        return output_text #type: ignore
     
     def convert_hero_variable_definition_to_golang_variable_definition(self, old_text: str) -> str:
         def try_to_do_a_replace_1(match_obj: Any):
@@ -116,7 +192,7 @@ func {function_name}({function_input_arguments}) {function_return_type} {{
                 variable_type = match_obj.group('variable_type')
                 variable_name = match_obj.group('variable_name')
                 return f'{indent} var {variable_name} {variable_type} ='
-        result = re.sub(r"^(?P<indent>[ \t]*)(?P<variable_type>\w+) (?P<variable_name>\w+) =", try_to_do_a_replace_1, old_text, flags=re.MULTILINE) #type: ignore
+        result = re.sub(r"^(?P<indent>[ \t]*)(?P<variable_type>[\w\[\]\.]+) (?P<variable_name>\w+) =", try_to_do_a_replace_1, old_text, flags=re.MULTILINE) #type: ignore
 
         def try_to_do_a_replace_2(match_obj: Any):
             if match_obj.group() is not None:
@@ -124,7 +200,7 @@ func {function_name}({function_input_arguments}) {function_return_type} {{
                 variable_type = match_obj.group('variable_type')
                 variable_name = match_obj.group('variable_name')
                 return f'{indent} var {variable_name} {variable_type}'
-        result = re.sub(r"^(?P<indent>[ \t]*)(?P<variable_type>\w+) (?P<variable_name>\w+)$", try_to_do_a_replace_2, result, flags=re.MULTILINE) #type: ignore
+        result = re.sub(r"^(?P<indent>[ \t]*)(?P<variable_type>[\w\[\]\.]+) (?P<variable_name>\w+)$", try_to_do_a_replace_2, result, flags=re.MULTILINE) #type: ignore
 
         return result #type: ignore
     
@@ -139,25 +215,19 @@ func {function_name}({function_input_arguments}) {function_return_type} {{
                 module_path = module_name
                 if module_name.startswith("./") or module_name.startswith("/"):
                     # it is a file path
-                    if module_name.startswith("/"):
-                        # it is an absolute path, ignore it
-                        pass
-                    # else:
-                    #     module_path = disk.join_relative_paths(base_dir, module_path)
+                    if module_nickname != None:
+                        module_name = module_nickname
+                    else:
+                        module_name, _ = disk.get_stem_and_suffix_of_a_file(module_name)
+                    package_name_list.append(module_name)
                 else:
                     # it is go package or hero package
                     # if it is hero package: you need to find the real path from 'hero_modules' folder
-                    if module_nickname != None:
-                        return f'import {module_nickname} "{module_name}"'
-                    else:
-                        return f'import "{module_name}"'
+                    if module_nickname == None:
+                        module_nickname = module_name
+                    package_name_list.append(module_name)
+                    return f'import {module_nickname} "{module_name}"'
 
-                if module_nickname != None:
-                    module_name = module_nickname
-                else:
-                    module_name, _ = disk.get_stem_and_suffix_of_a_file(module_name)
-                package_name_list.append(module_name)
-                
                 file = disk.get_absolute_path(path=module_path)
                 directory_path = disk.get_directory_path(path=file)
                 self.compile_to_golang_file(input_base_folder=directory_path, input_file=file, output_folder=output_folder, parent_package_path=parent_package_path)
@@ -181,10 +251,13 @@ func {function_name}({function_input_arguments}) {function_return_type} {{
             # create a package
             package_hash = disk.get_hash_of_a_path(disk.get_absolute_path(input_file))[:15]
             package_path = f"{package_hash}/{output_pure_file_name}"
-            terminal.run(f"""
-            export GO111MODULE=on
-            go mod init {package_path}
-            """, cwd=output_folder)
+            go_mod_path = disk.join_paths(output_folder, "go.mod")
+            if not disk.exists(go_mod_path):
+                terminal.run(f"""
+                export GO111MODULE=on
+                #rm go.mod
+                go mod init {package_path}
+                """, cwd=output_folder)
             parent_package_path = package_path
 
         input_code = io_.read(file_path=input_file)
@@ -198,7 +271,13 @@ func {function_name}({function_input_arguments}) {function_return_type} {{
         output_code = self.convert_hero_package_function_access_method_to_golang_package_function_access_method(old_text=output_code, package_name_list=package_name_list)
         output_code = self.convert_hero_variable_definition_to_golang_variable_definition(old_text=output_code)
 
-        output_code = f"""package {output_pure_file_name}\n\n\n""" + output_code
+        output_code = f"""
+// stupid golang doesn't allow unused variable exists, which is bad for coding experience
+// proposal: spec: make unused variables not an error #43729: https://github.com/golang/go/issues/43729
+
+package {output_pure_file_name}\n\n\n""".lstrip() + output_code
+
+
         # if package_nickname == None:
         #     output_code = f"""package {output_pure_file_name}\n\n\n""" + output_code
         # else:
@@ -211,12 +290,22 @@ func {function_name}({function_input_arguments}) {function_return_type} {{
 
         return output_file_path
 
-    def compile_to_binary_file(self, input_go_file: str, output_binary_file: str) -> str:
+    def compile_to_binary_file(self, input_go_file: str, output_binary_file: str, operation_system: str | None = None, architecture: str | None = None) -> str:
         base_folder = disk.get_directory_path(input_go_file)
-        terminal.run(f"""
-        export GO111MODULE=on
-        go build -o {output_binary_file} {input_go_file} 
-        """, cwd=base_folder)
+
+        if operation_system != None and architecture != None:
+            terminal.run(f"""
+            export GOOS={operation_system} 
+            export GOARCH={architecture}
+            export GO111MODULE=on
+            go build -o {output_binary_file} {input_go_file} 
+            """, cwd=base_folder)
+        else:
+            terminal.run(f"""
+            export GO111MODULE=on
+            go build -o {output_binary_file} {input_go_file} 
+            """, cwd=base_folder)
+
         return output_binary_file
 
 
